4f12c8e366c791fc2fe07a561d9c7faea148635e
Shahad-Alhabsi <csshahad00@gmail.com>
2023-11-14 13:54:04 +0400

    format lecture

---
+++ lecture notes.py
@@ -0,0 +1,24 @@
+# st = "adshahadfgh"
+# print(st.find("shahad",2,8))
+# 
+# st = "adshahadfgh"
+# print(st.rfind("shahad"))
+# 
+# st = "adshahadfghshahad"
+# print(st.rfind("shahad"))
+
+# x = "a"
+# print(ord(x), chr(97))
+# 
+# year = int(input("Please enter your year of birh: "))
+# print("You are ", (2023-year), " years old")
+
+# price = 1.23456
+# print("The price is %10.2f"%(price))
+
+# x = int(input("Enter an integer: "))
+# print("%04d"%(x))
+
+
+name, age = input("Enter your name and age: ").split()
+print("%s%5s"%(name, age))
