baec61c561e6bc32863c84f7791c9e18d6772c6c
Shahad-Alhabsi <csshahad00@gmail.com>
2023-11-14 11:57:06 +0400

    tiles problem

---
+++ tiles.py
@@ -0,0 +1,13 @@
+from math import *
+TOTAL_WIDTH = 147
+TILE_WIDTH = 5
+
+
+tiles = TOTAL_WIDTH / TILE_WIDTH # posible tiles number
+
+black_tiles = ceil(tiles/2)
+white_tiles = black_tiles - 1
+print("black tiles = {} \nwhite tiles = {}".format(black_tiles, white_tiles))
+
+gap = (TOTAL_WIDTH - (black_tiles+white_tiles)*5)/2
+print(gap)
